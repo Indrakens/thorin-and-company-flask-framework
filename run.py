71db6189fca48ff5db2410961e699d2b9c8712bd
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-07-30 15:25:42 +0000

    Add contact

--- run.py
+++ run.py
@@ -10,7 +10,12 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")    
+    return render_template("about.html")   
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")    
+
 
 
 if __name__ == "__main__":
